b69353ce81d424f26584780db0f4f292cbdd899a
Elmer fernandez <elmerfer@gmail.com>
2022-05-13 15:25:13 -0300

    runtrim

--- R/Trimgallore.R
+++ R/Trimgallore.R
@@ -1,5 +1,13 @@
 #Install Trim Gallore
-
+#'InstallTrimGalore
+#'@description 
+#'Install [Trimgalore](https://www.bioinformatics.babraham.ac.uk/projects/trim_galore/) priori to check the availability of [cutapat](/cutadapt.readthedocs.io/en/stable/installation.html)
+#'it will install Trimgalore in the Software directory handle by [GenomeDB] library
+#'@export
+#'@uses
+#'/dontrun{
+#'InstallTrimGalore
+#'}
 InstallTrimGalore <- function(){
   cut.path <- unlist(stringr::str_split(system2("whereis","cutadapt",stdout=T),":"))
   if(cut.path[2]==""){
@@ -35,7 +43,7 @@ InstallTrimGalore <- function(){
   software$Software$TrimGalore$main <- file.path(software$Software$main, basename(zfiles[1]))
   software$Software$TrimGalore$command <- file.path(software$Software$main, zfiles[stringr::str_detect(zfiles,"/trim_galore")])
   version<-system2(software$Software$TrimGalore$command,"--version", stdout = T)
-  if(all(stringr::str_detect(version,"version 0.6.6"))==FALSE){
+  if(all(stringr::str_detect(version,"version 0.6.6")==FALSE)==TRUE){
     
     file.remove(tmp.file)
     stop("TrimGalore  installation FAILED")
@@ -46,3 +54,25 @@ InstallTrimGalore <- function(){
   GenomeDB:::.OpenConfigFile(software)
 }
 
+#' RunTrimgalore
+#' RunTrimgalore(fileR1)
+#' @description RunTrimgalores is intended to perform adapters and trimming of read from paired end sequencing data
+#' @param fileR1 the fastq file read 1 (the read 2 will be automatically called)
+#' @export
+RunTrimgalore <- function(fileR1){
+  software <- GenomeDB:::.OpenConfigFile()
+  fileR2 <- stringr::str_replace_all(fileR1, "_R1.","_R2.")
+  fileR2 <- stringr::str_replace_all(fileR1, "_1.","_2.")
+  if(file.exists(software$Software$TrimGalore$command)==FALSE){
+    stop("Trimgalore is not installed, please see InstallTtrimgalore()")
+  }
+  t1<- Sys.time()
+  system2(command = software$Software$TrimGalore$command,
+          args =c("--paired",paste0("--output_dir ",dirname(fileR1)),fileR1,fileR2))
+  etime <- as.numeric(Sys.time()-t1,units="hours")
+  of.size1 <-file.info(fileR1)$size
+  ot.size1 <-file.info(paste0(unlist(stringr::str_split(fileR1,".f"))[1],"_val_1.fq.gz"))$size
+  of.size2 <-file.info(fileR2)$size
+  ot.size2 <-file.info(paste0(unlist(stringr::str_split(fileR2,".f"))[1],"_val_2.fq.gz"))$size
+  return(c(Time= etime, osize1=of.size1,osize2=of.size1,tsize1=ot.size1,tsize2=ot.size2))
+}
