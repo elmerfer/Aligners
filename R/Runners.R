67929db2f99b0bbc95ece6c11bf121ca854f6b3a
Elmer fernandez <elmerfer@gmail.com>
2022-05-31 14:33:41 -0300

    update

--- R/Runners.R
+++ R/Runners.R
@@ -3,9 +3,15 @@
 #'@param sbjFile string full path to the R1.fastq or or gz file
 #'@param species the species you whant to use, it should be stored in your database pls see \code{\link[GenomeDB]{ShowSpecies}}
 #'@param version the code indicating the genome+annotation pair
-#'@param twoPass "None" or "Basic"
+#'@param twoPass "None" or "Basic". If set to "Basic", Annotated junctions will be included in both the 1st and 2nd passes. To run STAR 2-pass mapping for
+#'each sample separately. STAR will perform the 1st pass mapping,
+#'then it will automatically extract junctions, insert them into the genome index, and, finally, re-map
+#'all reads in the 2nd mapping pass. This option can be used with annotations, which can be included
+#'either at the run-time or at the genome generation step.
 #'@param nThreads integer, number of CPUs to be used
 #'@export
+#'@return the file name of the resulted bam file with an attribute "assemblyVersion" that shows the reference-annotation verision 
+#'see \code{\link[GenomeDB]{GetGenomesInDB}}
 #'
 RunSTAR <- function(sbjFile, species, version, twoPass = c("None","Basic"), nThreads){
   if(require(GenomeDB)==FALSE){
@@ -99,6 +105,8 @@ RunSTAR <- function(sbjFile, species, version, twoPass = c("None","Basic"), nThr
 #'@param version the code indicating the genome+annotation pair
 #'@param nThreads integer, number of CPUs to be used
 #'@export
+#'@return the file name of the resulted bam file with an attribute "assemblyVersion" that shows the reference-annotation verision 
+#'see \code{\link[GenomeDB]{GetGenomesInDB}}
 #'
 RunSubjunct <- function(sbjFile, species, version, nThreads){
   if(require(GenomeDB)==FALSE){
@@ -114,9 +122,13 @@ RunSubjunct <- function(sbjFile, species, version, nThreads){
   file1 <- sbjFile
   if(stringr::str_detect(file1, "_R1.fastq")){
     file2 <- stringr::str_replace(file1,"_R1.fastq","_R2.fastq")
+    outf <- stringr::str_remove_all(file1,"_R1.fastq|_R1.fastq.gz")
+    out.file <- paste0(outf,"_Rsubread_alignment.bam")
   }else{
     if(stringr::str_detect(file1, "_1.fastq")){
       file2 <- stringr::str_replace(file1,"_1.fastq","_2.fastq")
+      outf <- stringr::str_remove_all(file1,"_1.fastq|_1.fastq.gz")
+      out.file <- paste0(outf,"_Rsubread_alignment.bam")
     }else{
       stop("unrecognizable file, it should be sbj_R1.fastq/.gz or sbj_1.fastq/.gz")
     }
@@ -146,9 +158,15 @@ RunSubjunct <- function(sbjFile, species, version, nThreads){
   
   
   
-  Rsubread::subjunc(index = genomeDir,
+  stats <- Rsubread::subjunc(index = genomeDir,
                     readfile1 = file1,
                     readfile2 = file2,
                     nthreads = nThreads,
-                    reportAllJunctions = TRUE)
+                    reportAllJunctions = TRUE,
+                    output_file = out.file)
+  if(!file.exists(out.file)){
+    stop(paste0("ERROR: file not created " ,out.file ))
+  }
+  attr(out.file,"assemblyVersion") <- version
+  return(out.file)
 }
--- R/Trimgallore.R
+++ R/Trimgallore.R
@@ -4,8 +4,8 @@
 #'Install [Trimgalore](https://www.bioinformatics.babraham.ac.uk/projects/trim_galore/) priori to check the availability of [cutapat](/cutadapt.readthedocs.io/en/stable/installation.html)
 #'it will install Trimgalore in the Software directory handle by [GenomeDB] library
 #'@export
-#'@uses
-#'/dontrun{
+#'@usage
+#'\dontrun{
 #'InstallTrimGalore
 #'}
 InstallTrimGalore <- function(){
@@ -57,22 +57,60 @@ InstallTrimGalore <- function(){
 #' RunTrimgalore
 #' RunTrimgalore(fileR1)
 #' @description RunTrimgalores is intended to perform adapters and trimming of read from paired end sequencing data
-#' @param fileR1 the fastq file read 1 (the read 2 will be automatically called)
+#' @param fileR1 the fastq file read 1 (the read 2 will be automatically called) accepted files are XXX_1.fastq/_r1.fastq or fq.gz. Their paired files should replace the _1 (_R1) by _2 (_R2)
+#' @outdir if missing the current directory will be used. It will be created if does not exists
+#' @param replace (boolean) If TRUE the original files will be reomved and replaced by the trimmed ones under the same original name but gziped (.fq.gz), otherwise it will created two new files 
+#' as XXX_1_val_1.fq.gz and XXX_2_val_2,fq.gz
 #' @export
-RunTrimgalore <- function(fileR1){
+#' @usage 
+#' \dontrun{
+#' file1 <- "my.file_1.fastq #it is expected to have their pair as ny.file_2.fastq
+#' res <- RunTrimgalore(file1)
+#' res ##this will hold the file1 name and the elapsed time as well as the file sizes prior and after trimming
+#' }
+RunTrimgalore <- function(fileR1, outdir, replace=FALSE){
   software <- GenomeDB:::.OpenConfigFile()
   fileR2 <- stringr::str_replace_all(fileR1, "_R1.","_R2.")
   fileR2 <- stringr::str_replace_all(fileR1, "_1.","_2.")
   if(file.exists(software$Software$TrimGalore$command)==FALSE){
     stop("Trimgalore is not installed, please see InstallTtrimgalore()")
   }
+  
+  gziped <- ifelse(stringr::str_detect(fileR1,".gz"),"--gzip","--dont_gzip")
+  outdir <- ifelse(missing(outdir), paste0("--output_dir ",dirname(fileR1)),paste0("--output_dir ",file.path(dirname(fileR1),outdir)))
+  
   t1<- Sys.time()
   system2(command = software$Software$TrimGalore$command,
-          args =c("--paired",paste0("--output_dir ",dirname(fileR1)),fileR1,fileR2))
+          args =c("--paired",
+                  gziped,
+                  outdir,
+                  fileR1,
+                  fileR2))
+  
   etime <- as.numeric(Sys.time()-t1,units="hours")
+  
   of.size1 <-file.info(fileR1)$size
-  ot.size1 <-file.info(paste0(unlist(stringr::str_split(fileR1,".f"))[1],"_val_1.fq.gz"))$size
   of.size2 <-file.info(fileR2)$size
-  ot.size2 <-file.info(paste0(unlist(stringr::str_split(fileR2,".f"))[1],"_val_2.fq.gz"))$size
-  return(c(file=basename(fileR1),Time= etime, osize1=of.size1,osize2=of.size1,tsize1=ot.size1,tsize2=ot.size2))
+  if(gziped=="--gzip"){
+    ofile <- paste0(unlist(stringr::str_split(basename(fileR1),".f"))[1],"_val_1.fq.gz")
+    file.rename(ofile,stringr::str_replace_all(ofile,"_1_|_R1_",""))
+    ofile <-stringr::str_replace_all(ofile,"_1_|_R1_","")
+    ot.size1 <-file.info(file.path(outdir,ofile))$size  
+    of2 <- paste0(unlist(stringr::str_split(basename(fileR2),".f"))[1],"_val_2.fq.gz")
+    file.rename(of2,stringr::str_replace_all(of2,"_2_|_R2_",""))
+    ot.size2 <-file.info(file.path(outdir,of2))$size
+  }else{
+    ofile <- paste0(unlist(stringr::str_split(basename(fileR1),".f"))[1],"_val_1.fastq")
+    file.rename(ofile,stringr::str_replace_all(ofile,"_1_|_R1_",""))
+    ofile <-stringr::str_replace_all(ofile,"_1_|_R1_","")
+    ot.size1 <-file.info(file.path(outdir,paste0(unlist(stringr::str_split(basename(fileR1),".f"))[1],"_val_1.fastq")))$size  
+    of2 <- paste0(unlist(stringr::str_split(basename(fileR2),".f"))[1],"_val_2.fastq")
+    file.rename(of2,stringr::str_replace_all(of2,"_2_|_R2_",""))
+    ot.size2 <-file.info(file.path(outdir,of2))$size    
+  }
+  
+  
+  
+  
+  return(c(ofile=fileR1,tfile = ofile ,Time= etime, osize1=of.size1,osize2=of.size1,tsize1=ot.size1,tsize2=ot.size2))
 }
